d19a34630e058a13db7a47a5fb6782dd35f19143
jenny72 <yiennee93@gmail.com>
2024-07-05 11:50:12 +0800

    20240705_JungleGym

---
+++ jungle-gym/JungleGym/CogJobResultRetriever.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace JungleGym
+{
+    public class CogJobResultRetriever : QVS_Interfaces.IResult
+    {
+        XDocument settingXml;
+        string ResultXmlPath;
+        public CogJobResultRetriever(XDocument doc)
+        {
+            settingXml = doc;
+            ResultXmlPath = settingXml.Root.Element("ResultXmlPath").Value;
+        }
+
+        public void OutputResult(object[] parameters, out string sErrMsg)
+        {
+            sErrMsg = "";
+
+            ProcessResult(parameters);
+        }
+
+        public void ProcessResult(object data)
+        {
+
+        }
+    }
+}
